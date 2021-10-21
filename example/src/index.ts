92bdaf0dc38fdf9f03842a12271b2ac52111a32d
Brad Garropy <bradgarropy@gmail.com>
2021-10-21 17:14:37 -0500

    update example.

--- example/src/index.ts
+++ example/src/index.ts
@@ -4,7 +4,7 @@ import Captivate from "../../."
 
 dotenv.config()
 
-// const SHOW_ID = "bae7a2f1-8cf5-46aa-b566-243bfbd837f7"
+const SHOW_ID = "bae7a2f1-8cf5-46aa-b566-243bfbd837f7"
 // const EPISODE_ID = "82b91907-6d18-4fef-9aaf-7bd8273b75fc"
 
 const captivate = new Captivate(
@@ -28,11 +28,11 @@ const captivate = new Captivate(
 //         console.log(shows)
 //     })
 
-captivate.users
-    .getUsersManagedShows(process.env.CAPTIVATE_USER_ID as string)
-    .then(managedShows => {
-        console.log(managedShows)
-    })
+// captivate.users
+//     .getUsersManagedShows(process.env.CAPTIVATE_USER_ID as string)
+//     .then(managedShows => {
+//         console.log(managedShows)
+//     })
 
 // shows
 
@@ -48,7 +48,9 @@ captivate.users
 //     console.log(episodes)
 // })
 
-// captivate.shows.getShowScheduledEpisodes()
+captivate.shows.getShowScheduledEpisodes(SHOW_ID).then(scheduledEpisodes => {
+    console.log(scheduledEpisodes)
+})
 
 // captivate.shows.getShowFeedUrl()
 
