005895fa3b67843704d167ec97397de6e03c33c6
Brad Garropy <bradgarropy@gmail.com>
2021-10-22 14:37:05 -0500

    add tests.

--- tests/media.test.ts
+++ tests/media.test.ts
@@ -1,14 +1,19 @@
+import fetch from "jest-fetch-mock"
+
 import {
     getMedia,
     getShowMedia,
     searchShowMedia,
     uploadMedia,
 } from "../src/media"
+import {mockGetMediaResponse, mockMedia} from "./mocks"
 
 describe("media", () => {
-    test("gets media", () => {
-        const result = getMedia()
-        expect(result).toBeUndefined()
+    test("gets media", async () => {
+        fetch.mockResponseOnce(JSON.stringify(mockGetMediaResponse))
+
+        const result = await getMedia("token", "abc123")
+        expect(result).toEqual(mockMedia)
     })
 
     test("uploads media", () => {
