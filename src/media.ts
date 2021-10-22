7fb7f90f541b7bb0edd5cd8bab9103305f42c862
Brad Garropy <bradgarropy@gmail.com>
2021-10-22 14:29:40 -0500

    add underlying logic.

--- src/media.ts
+++ src/media.ts
@@ -1,5 +1,20 @@
-const getMedia = () => {
-    // TODO
+import {API_URL} from "./constants"
+import {AuthenticatedUser, GetMediaResponse, Media} from "./types"
+
+const getMedia = async (
+    token: AuthenticatedUser["token"],
+    mediaId: Media["id"],
+): Promise<Media> => {
+    const response = await fetch(`${API_URL}/media/${mediaId}`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        redirect: "follow",
+    })
+
+    const json: GetMediaResponse = await response.json()
+    return json.media
 }
 
 const uploadMedia = () => {
